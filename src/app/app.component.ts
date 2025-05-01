eb8acf2540329be973171db1fb63b13532d412e5
mayarelgarf <mayarmahmoud123@gmail.com>
2025-05-01 15:36:02 +0300

    update side nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component, DestroyRef, inject, signal, ViewChild } from '@angular/core';
+import {
+  Component,
+  inject,
+  signal,
+  ViewChild,
+} from '@angular/core';
 import { BreakpointObserver, MediaMatcher } from '@angular/cdk/layout';
 import { MatSidenav } from '@angular/material/sidenav';
-import { filter } from 'rxjs/operators';
-import { NavigationEnd, Router } from '@angular/router';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { NavLink } from './interfaces/nav.interface';
 import { MainAppPaths } from './enums/MainAppPaths.enum';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,27 +19,31 @@ import { MainAppPaths } from './enums/MainAppPaths.enum';
 export class AppComponent {
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
-navLinks:NavLink[]=[
-  {title:'Home',link:`${MainAppPaths.HOME}`,icon:'home'},
-  {title:'person',link:'',icon:'person'},
-  {title:'Notifications',link:`${MainAppPaths.NOTIFICATION}`,icon:'notifications'},
-]
+  navLinks: NavLink[] = [
+    { title: 'Home', link: `${MainAppPaths.HOME}`, icon: 'home' },
+    { title: 'person', link: '', icon: 'person' },
+    {
+      title: 'Notifications',
+      link: `${MainAppPaths.NOTIFICATION}`,
+      icon: 'notifications',
+    },
+  ];
 
-protected readonly isMobile = signal(true);
+  protected readonly isMobile = signal(true);
 
-private readonly _mobileQuery: MediaQueryList;
-private readonly _mobileQueryListener: () => void;
 
-constructor() {
-  const media = inject(MediaMatcher);
+  private breakpointSub: Subscription;
+  private breakpointObserver = inject(BreakpointObserver);
 
-  this._mobileQuery = media.matchMedia('(max-width: 600px)');
-  this.isMobile.set(this._mobileQuery.matches);
-  this._mobileQueryListener = () => this.isMobile.set(this._mobileQuery.matches);
-  this._mobileQuery.addEventListener('change', this._mobileQueryListener);
-}
+  constructor() {
+    this.breakpointSub = this.breakpointObserver
+      .observe(['(max-width: 600px)'])
+      .subscribe(result => {
+        this.isMobile.set(result.matches);
+      });
+  }
 
-ngOnDestroy(): void {
-  this._mobileQuery.removeEventListener('change', this._mobileQueryListener);
-}
+  ngOnDestroy(): void {
+    this.breakpointSub.unsubscribe();
+  }
 }
